810cf5f90931678a987022f7deeb1c178c127b10
spirtual-asthma <hello@denisemensah.com>
2020-08-01 10:06:39 -0400

    progress

---
+++ gatsby-site/old/includes/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    error_reporting( E_ALL & ~E_NOTICE );
+
+    ini_set( 'display_errors', true );
+
+    date_default_timezone_set( 'America/Toronto' );
+
+    define( 'DB_HOST',      'localhost' );
+    define( 'DB_USER',      'denisem_portfolio' );
+    define( 'DB_PASSWORD',  'MakeitHappen17%' );
+    define( 'DB_NAME',      'denisem_portfolio' );
+    define( 'SITE_ROOT', 'http://denisemensah.com/' );
---
+++ real-site/gatsby-starter-default/src/components/header.js
@@ -0,0 +1,39 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header>
+  <div className="nav">
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Portfolio</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+    <div className="box">
+        <div className="viewport-hero"></div>
+      <div className="tagline-box">
+      <h1 className="tagline">
+          {siteTitle}
+      </h1>
+      </div>
+      </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
